53894d2045b31fa907112a2b601dbf51abc8a700
modotz <modotzprakoso@gmail.com>
2023-05-30 10:29:55 +0700

    root api

--- telepati-server/app.js
+++ telepati-server/app.js
@@ -52,8 +52,8 @@ global.ServerHost = "http://localhost"; // rumah
 
 var port = process.env.PORT || 1987;
 app.set("port", port);
-//const server = http.createServer(app);
-const server = https.createServer(options, app);
+const server = http.createServer(app);
+//const server = https.createServer(options, app);
 const io = require("socket.io")(server);
 
 //app.use(logger('dev'));
@@ -235,21 +235,21 @@ io.on("connection", (socket) => {
   });
 });
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 server.listen(port, () => {
   console.log(`listening on ${ServerHost}:${port}`);
