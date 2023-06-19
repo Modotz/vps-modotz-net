36c172f2e9f8219d8ce1d03f818cd8e99c646c22
modotz <modotzprakoso@gmail.com>
2023-06-19 10:54:55 +0700

    change turn server

--- telepati-server/public/scripts/webRTCHandler.js
+++ telepati-server/public/scripts/webRTCHandler.js
@@ -12,32 +12,32 @@ const configuration = {
     {
       urls: "stun:openrelay.metered.ca:80",
     },
-    {
-      urls: "turn:143.198.86.130:3478",
-      username: "modotz",
-      credential: "modotz1234",
-    },
-    {
-      urls: "turn:143.198.86.130:443",
-      username: "modotz",
-      credential: "modotz",
-    },
+    // {
+    //   urls: "turn:143.198.86.130:3478",
+    //   username: "modotz",
+    //   credential: "modotz1234",
+    // },
+    // {
+    //   urls: "turn:143.198.86.130:443",
+    //   username: "modotz",
+    //   credential: "modotz",
+    // },
 
-    // {
-    //   urls: "turn:openrelay.metered.ca:80",
-    //   username: "openrelayproject",
-    //   credential: "openrelayproject",
-    // },
-    // {
-    //   urls: "turn:openrelay.metered.ca:443",
-    //   username: "openrelayproject",
-    //   credential: "openrelayproject",
-    // },
-    // {
-    //   urls: "turn:openrelay.metered.ca:443?transport=tcp",
-    //   username: "openrelayproject",
-    //   credential: "openrelayproject",
-    // },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
 
 
   ],
